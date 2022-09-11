74943b29a4af4fefa93705aae4c85b9f87ee7c4e
Carlos Garcia <alu0101208268.ull.edu.es>
2022-09-11 12:00:20 +0100

    showing diferents gif

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 
 const GIFS =['https://media4.giphy.com/media/ssYuHGAAz8FMrFF0JN/giphy.gif?cid=ecf05e478e5ldzp4wbxap37k6x65i00thw4u4qomxqp5igrv&rid=giphy.gif&ct=g','https://media0.giphy.com/media/l41JU9pUyosHzWyuQ/giphy.gif?cid=ecf05e478e5ldzp4wbxap37k6x65i00thw4u4qomxqp5igrv&rid=giphy.gif&ct=g'];
 
+
+
 const App = () => {
   const [gifs, setGifs] = useState(GIFS);
 
   return (
     <div className="App">
       <section className='App-content'>
-        <img src={gifs}/>
+      {
+        gifs.map(gif => <img src={gif} alt='giff'/>)
+      }
       </section>
     </div>
   );
