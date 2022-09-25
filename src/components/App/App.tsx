39ccbc8dfceddeb3bfb615ec5b8f8676cd9262f5
Carlos Garcia <alu0101208268.ull.edu.es>
2022-09-25 22:09:57 +0100

    changin root name

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
               path='/search/:keyword'/>
             <Route 
               component={Home} 
-              path='/'/>
+              path='/Giffy'/>
             <Route 
               component={DetailView} 
               path='/gif/:id'/>
